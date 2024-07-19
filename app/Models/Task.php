bb1e9ba05790073e6cd57bbe8456d208b61b272c
norainibpm <noraini.talib@ekonomi.gov.my>
2024-07-19 15:00:10 +0800

    day5

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,46 +1,30 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public $timestamps = true;
 
     protected $table = 'tasks';
-
     protected $primaryKey = 'id';
-
     public $incrementing = true;
-
     protected $guarded = ['id'];
-
     function getRouteKeyName() {
         return 'uuid';
     }
-
-    /**
-     * Get the user that owns the Task
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
-    /**
-     * Get all of the comments for the Task
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
     public function comments()
     {
         return $this->hasMany(Comment::class, 'task_id', 'id');
     }
-
 }
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,43 +4,43 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 @endsection
 
-@section('pagetitle','Create')
+@section('pagetitle','Create Task')
 
 @section('breadcrumb')
 <li class="breadcrumb-item"><a href="javascript: void(0);">Todolist</a></li>
-<li class="breadcrumb-item active">Tajuk</li>
+<li class="breadcrumb-item active">Create Task</li>
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">New Task Form</div>
-                <div class="card-body">
-                   <form method="POST" name="createForm" id="createForm" action="{{ route('tasks.store')}}" >
-                       @csrf
-                       @include('tasks._form')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">New Task Form</div>
+                    <div class="card-body">
+                        <form method="POST" name="createForm" id="createForm" action="{{ route('tasks.store') }}" >
+                            @csrf
+                            @include('tasks._form')
 
-                       <div class="btn-group float-right">
-                           <button type="reset" class="btn btn-warning">Reset</button>
-                           <button type="submit" class="btn btn-success">Add</button>
-                       </div>
+                            <div class="btn-group float-right">
+                                <button type="reset" class="btn btn-warning">Reset</button>
+                                <button type="submit" class="btn btn-success">Add</button>
+                            </div>
 
-                   </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('script')
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
     $('#user_id').select2({
-        placeholder: "Sila Pilih pengguna",
+        placeholder: "Sila pilih pengguna",
         allowClear: true
     });
-    </script>
+</script>
 @endsection
